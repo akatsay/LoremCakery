abbc3c1cf5084c6eed803f9a873c09a9bd5c535e
a_katsay a_katsay <a_katsay@mail.ru>
2023-04-18 21:52:43 -0400

    add sorting gallery items

--- client/src/pages/galleryPage/galleryPage.js
+++ client/src/pages/galleryPage/galleryPage.js
@@ -11,9 +11,19 @@ export const GalleryPage = () => {
 
     const {loading, request} = useHttp()
 
-    const [items, setItems] = useState("")
+    const [items, setItems] = useState([])
+    const [sortState, setSortState] = useState("none");
     const [openCreateAreaModal, setOpenCreateAreaModal] = useState(false)
 
+    const sortMethods = {
+        none: { method: (a, b) => new Date(a.date) - new Date(b.date) },
+        ascending: { method: (a, b) => a.price - b.price },
+        descending: { method: (a, b) => b.price - a.price },
+      };
+
+    const handleSetSortMethod = (method) => {
+        setSortState(method)
+    }
 
     const fetchItems = useCallback( async () => {
         try {
@@ -22,11 +32,11 @@ export const GalleryPage = () => {
         } catch (e) {
             console.log(e.message)
         }
-    })
+    }, [request])
 
     useEffect(() => {
         fetchItems()
-    }, [])
+    }, [fetchItems])
 
     const {isAdmin} = useContext(AuthContext)
 
@@ -39,28 +49,35 @@ export const GalleryPage = () => {
             >
                 <div className="page-container">
                     {loading ? <SmallLoader /> :
-                    <div className="gallery-items-container">
-                        {isAdmin && 
-                        <div
-                        className="create-area-trigger"
-                        onClick={() => setOpenCreateAreaModal(true)}
-                        >
-                            <div className='plus-sign'>+</div>
-                            <p>Add Item</p>
-                        </div>    
-                        }
-                        {items && items.map((item) => 
-                            <GalleryItem 
-                                key={item._id}
-                                id={item._id}
-                                imageSrc={item.url}
-                                title={item.title}
-                                description={item.description}
-                                price={item.price}
-                                fetchItems={fetchItems}
-                            />
-                        )}
-                    </div>
+                    <>
+                        <div className='sort-settings'>
+                            <i className="fa fa-solid fa-clock-rotate-left" onClick={() => handleSetSortMethod("none")}> reset</i>
+                            <i className="fa fa-sharp fa-solid fa-arrow-up-short-wide" onClick={() => handleSetSortMethod("ascending")}> price-up</i>
+                            <i className="fa fa-sharp fa-solid fa-arrow-down-short-wide" onClick={() => handleSetSortMethod("descending")}> price-down</i>  
+                        </div>
+                        <div className="gallery-items-container">
+                            {isAdmin && 
+                            <div
+                            className="create-area-trigger"
+                            onClick={() => setOpenCreateAreaModal(true)}
+                            >
+                                <div className='plus-sign'>+</div>
+                                <p>Add Item</p>
+                            </div>    
+                            }
+                            {items && items.sort(sortMethods[sortState].method).map((item) => 
+                                <GalleryItem 
+                                    key={item._id}
+                                    id={item._id}
+                                    imageSrc={item.url}
+                                    title={item.title}
+                                    description={item.description}
+                                    price={item.price}
+                                    fetchItems={fetchItems}
+                                />
+                            )}
+                        </div>
+                    </>
                     }   
                 </div>
             </motion.div>
